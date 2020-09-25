3fb09310130f98bad658bd4ccc68fbb81f0812d8
joonnoh <71247743+joonnoh@users.noreply.github.com>
2020-09-25 15:01:07 -0400

    Create commaCode.py

---
+++ commaCode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Comma Code program takes list and returns readable string
+
+# Initialize the list
+yourList = ['spam', 'eggs', 'apple', 'banana']
+
+# Initialize the final string to be returned
+finalString = ''
+
+if not yourList: # If the list is empty, let the user know 
+    print("List is empty")
+else:
+    for n in range(0,len(yourList)):             # Iterate through the list and append item and comma to finalString
+        if n == len(yourList)-1:                 # At the end of the list, add 'and' before the last item
+            finalString += "and " + yourList[n]
+        else:
+            finalString += yourList[n] + ", "
+print(finalString)
